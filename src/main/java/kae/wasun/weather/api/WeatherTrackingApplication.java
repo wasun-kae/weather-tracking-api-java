a8f78ad3ce950f8254151905d30d81db8de3792d
Wasun Kaewpradub <wasun.kaewpradub@gmail.com>
2024-08-05 14:00:08 +0700

    Add Spring Boot starter files

---
+++ src/main/java/kae/wasun/weather/api/WeatherTrackingApplication.java
@@ -0,0 +1,12 @@
+package kae.wasun.weather.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherTrackingApplication.class, args);
+	}
+}
---
+++ src/test/java/kae/wasun/weather/api/WeatherTrackingApplicationTests.java
@@ -0,0 +1,12 @@
+package kae.wasun.weather.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherTrackingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
