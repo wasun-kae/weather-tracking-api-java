11ae0f3c1d2a96bea053bc86368b478ba5d674d2
Wasun Kaewpradub <wasun.kaewpradub@gmail.com>
2024-08-05 16:10:50 +0700

    Add actuator health endpoint

---
+++ src/test/java/kae/wasun/weather/api/controller/ActuatorEndpointIT.java
@@ -0,0 +1,41 @@
+package kae.wasun.weather.api.controller;
+
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class ActuatorEndpointIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void should_return_status_ok_with_status_up_from_health_endpoint() throws Exception {
+        mockMvc.perform(get("/actuator/health"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {
+                            "status": "UP"
+                        }
+                        """)
+                );
+    }
+
+    @Test
+    void should_return_status_not_found_from_environment_endpoint() throws Exception {
+        mockMvc.perform(get("/actuator/env"))
+                .andExpect(status().isNotFound());
+    }
+}
