80b2823b861d5308ded27230b769a237007825ec
Wasun Kaewpradub <wasun.kaewpradub@gmail.com>
2024-08-06 16:50:17 +0700

    Add save method in device repository

--- src/main/java/kae/wasun/weather/api/repository/DeviceRepository.java
+++ src/main/java/kae/wasun/weather/api/repository/DeviceRepository.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.Expression;
 import software.amazon.awssdk.enhanced.dynamodb.Key;
+import software.amazon.awssdk.enhanced.dynamodb.model.GetItemEnhancedRequest;
+import software.amazon.awssdk.enhanced.dynamodb.model.PutItemEnhancedRequest;
 
 import java.text.MessageFormat;
 import java.util.Optional;
@@ -31,12 +34,49 @@ public class DeviceRepository {
                 .sortValue(sortKey)
                 .build();
 
-        var weatherTracking = dynamoDbTable.getItem(compositeKeys);
-        
-        return weatherTracking == null ? Optional.empty() :
-                Optional.of(Device.builder()
-                        .id(weatherTracking.getId())
+        var document = dynamoDbTable.getItem(compositeKeys);
+
+        return document == null ? Optional.empty() : Optional.of(
+                Device.builder()
+                        .id(document.getId())
                         .build()
-                );
+        );
+    }
+
+    public Device save(Device device) {
+        var partitionKey = MessageFormat.format("device#{0}", device.getId());
+        var sortKey = MessageFormat.format("device#{0}", device.getId());
+        var documentToSaved = WeatherTrackingDocument.builder()
+                .PK(partitionKey)
+                .SK(sortKey)
+                .id(device.getId())
+                .build();
+
+        var expression = Expression.builder()
+                .expression("attribute_not_exists(PK) AND attribute_not_exists(SK)")
+                .build();
+
+        var putItemRequest = PutItemEnhancedRequest.builder(WeatherTrackingDocument.class)
+                .item(documentToSaved)
+                .conditionExpression(expression)
+                .build();
+
+        dynamoDbTable.putItem(putItemRequest);
+
+        var compositeKeys = Key.builder()
+                .partitionValue(partitionKey)
+                .sortValue(sortKey)
+                .build();
+
+        var getItemRequest = GetItemEnhancedRequest.builder()
+                .key(compositeKeys)
+                .consistentRead(true)
+                .build();
+
+        var savedDocument = dynamoDbTable.getItem(getItemRequest);
+
+        return Device.builder()
+                .id(savedDocument.getId())
+                .build();
     }
 }
--- src/test/java/kae/wasun/weather/api/repository/DeviceRepositoryIT.java
+++ src/test/java/kae/wasun/weather/api/repository/DeviceRepositoryIT.java
@@ -5,20 +5,25 @@ import kae.wasun.weather.api.config.DynamoDBConfig;
 import kae.wasun.weather.api.config.TestContainersConfig;
 import kae.wasun.weather.api.helper.DynamoDBTestHelper;
 import kae.wasun.weather.api.model.document.WeatherTrackingDocument;
+import kae.wasun.weather.api.model.domain.Device;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.services.dynamodb.model.ConditionalCheckFailedException;
 
 import java.text.MessageFormat;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @Import({AwsConfig.class, DynamoDBConfig.class, TestContainersConfig.class})
 public class DeviceRepositoryIT {
+
+    private final String deviceId = "mock-device-id";
 
     @Autowired
     private DynamoDbEnhancedClient enhancedClient;
@@ -36,11 +41,11 @@ public class DeviceRepositoryIT {
 
         @Test
         void should_return_device_if_exists() {
-            var deviceId = "mock-device-id";
-            var deviceKey = MessageFormat.format("device#{0}", deviceId);
+            var partitionKey = MessageFormat.format("device#{0}", deviceId);
+            var sortKey = MessageFormat.format("device#{0}", deviceId);
             var document = WeatherTrackingDocument.builder()
-                    .PK(deviceKey)
-                    .SK(deviceKey)
+                    .PK(partitionKey)
+                    .SK(sortKey)
                     .id(deviceId)
                     .build();
 
@@ -53,10 +58,45 @@ public class DeviceRepositoryIT {
 
         @Test
         void should_return_empty_if_not_exist() {
-            var deviceId = "mock-device-id";
             var actual = deviceRepository.findById(deviceId);
 
             assertThat(actual.isEmpty()).isTrue();
+        }
+    }
+
+    @Nested
+    class save {
+
+        @Test
+        void should_save_new_device_if_not_exists() {
+            var deviceToSave = Device.builder()
+                    .id(deviceId)
+                    .build();
+
+            var actual = deviceRepository.save(deviceToSave);
+
+            assertThat(actual.getId()).isEqualTo(deviceId);
+        }
+
+        @Test
+        void should_throw_an_error_if_device_already_exists() {
+            var device = Device.builder()
+                    .id(deviceId)
+                    .build();
+
+            var partitionKey = MessageFormat.format("device#{0}", deviceId);
+            var sortKey = MessageFormat.format("device#{0}", deviceId);
+            var existingDocument = WeatherTrackingDocument.builder()
+                    .PK(partitionKey)
+                    .SK(sortKey)
+                    .id(deviceId)
+                    .build();
+
+            DynamoDBTestHelper.putItem(enhancedClient, existingDocument);
+
+            assertThatThrownBy(() ->
+                    deviceRepository.save(device)
+            ).isInstanceOf(ConditionalCheckFailedException.class);
         }
     }
 }
