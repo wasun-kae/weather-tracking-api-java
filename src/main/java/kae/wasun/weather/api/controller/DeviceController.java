b84c93bbf388d9ae74ec1b77c43a3271e964523e
Wasun Kaewpradub <wasun.kaewpradub@gmail.com>
2024-08-07 10:52:51 +0700

    Add create method in device controller

--- src/main/java/kae/wasun/weather/api/controller/DeviceController.java
+++ src/main/java/kae/wasun/weather/api/controller/DeviceController.java
@@ -1,9 +1,12 @@
 package kae.wasun.weather.api.controller;
 
+import kae.wasun.weather.api.model.dto.CreateDeviceDto;
 import kae.wasun.weather.api.model.dto.DeviceDto;
+import kae.wasun.weather.api.model.exception.ItemAlreadyExists;
 import kae.wasun.weather.api.model.exception.ItemNotFoundException;
 import kae.wasun.weather.api.service.DeviceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,5 +22,10 @@ public class DeviceController {
     public ResponseEntity<DeviceDto> findById(@PathVariable String id) throws ItemNotFoundException {
         var deviceDto = deviceService.findById(id);
         return ResponseEntity.ok().body(deviceDto);
+    }
+
+    public ResponseEntity<DeviceDto> create(CreateDeviceDto createDeviceDto) throws ItemAlreadyExists {
+        var deviceDto = deviceService.create(createDeviceDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(deviceDto);
     }
 }
--- src/test/java/kae/wasun/weather/api/controller/DeviceControllerTest.java
+++ src/test/java/kae/wasun/weather/api/controller/DeviceControllerTest.java
@@ -1,6 +1,8 @@
 package kae.wasun.weather.api.controller;
 
+import kae.wasun.weather.api.model.dto.CreateDeviceDto;
 import kae.wasun.weather.api.model.dto.DeviceDto;
+import kae.wasun.weather.api.model.exception.ItemAlreadyExists;
 import kae.wasun.weather.api.model.exception.ItemNotFoundException;
 import kae.wasun.weather.api.service.DeviceService;
 import org.junit.jupiter.api.*;
@@ -58,6 +60,45 @@ class DeviceControllerTest {
             deviceController.findById(deviceId);
 
             verify(deviceService, times(1)).findById(deviceId);
+        }
+    }
+
+    @Nested
+    class create {
+
+        @BeforeEach
+        void setUp() throws ItemAlreadyExists {
+            var createDeviceDto = CreateDeviceDto.builder().id(deviceId).build();
+            var deviceDto = DeviceDto.builder()
+                    .id(deviceId)
+                    .build();
+
+            when(deviceService.create(createDeviceDto)).thenReturn(deviceDto);
+        }
+
+        @Test
+        void should_return_status_201_when_a_new_device_created() throws ItemAlreadyExists {
+            var createDeviceDto = CreateDeviceDto.builder().id(deviceId).build();
+            var actual = deviceController.create(createDeviceDto);
+
+            assertThat(actual.getStatusCode().value()).isEqualTo(201);
+        }
+
+        @Test
+        void should_return_created_device() throws ItemAlreadyExists {
+            var createDeviceDto = CreateDeviceDto.builder().id(deviceId).build();
+            var actual = deviceController.create(createDeviceDto);
+
+            assertThat(actual.getBody()).isNotNull();
+            assertThat(actual.getBody().getId()).isEqualTo(deviceId);
+        }
+
+        @Test
+        void should_create_given_device() throws ItemAlreadyExists {
+            var createDeviceDto = CreateDeviceDto.builder().id(deviceId).build();
+            deviceController.create(createDeviceDto);
+
+            verify(deviceService, times(1)).create(createDeviceDto);
         }
     }
 }
