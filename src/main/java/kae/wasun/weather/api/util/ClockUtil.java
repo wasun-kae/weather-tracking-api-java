897521b52055bc633454302d7250b55911c3c23e
Wasun Kaewpradub <wasun.kaewpradub@gmail.com>
2024-08-09 11:35:19 +0700

    Add convert to string method in clock util

--- src/main/java/kae/wasun/weather/api/util/ClockUtil.java
+++ src/main/java/kae/wasun/weather/api/util/ClockUtil.java
@@ -3,11 +3,21 @@ package kae.wasun.weather.api.util;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 @Component
 public class ClockUtil {
 
+    private final DateTimeFormatter formatter = DateTimeFormatter
+            .ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+            .withZone(ZoneId.of("UTC"));
+
     public Instant getCurrentTime() {
         return Instant.now();
+    }
+
+    public String convertToString(Instant instant) {
+        return formatter.format(instant);
     }
 }
