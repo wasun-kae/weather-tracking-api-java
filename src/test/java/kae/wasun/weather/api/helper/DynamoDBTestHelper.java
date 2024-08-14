491dd2bb804bb7575f5cf3682bc973d6eba9e393
Wasun Kaewpradub <wasun.kaewpradub@gmail.com>
2024-08-14 12:31:30 +0700

    Refactor dynamodb test helper

--- src/test/java/kae/wasun/weather/api/helper/DynamoDBTestHelper.java
+++ src/test/java/kae/wasun/weather/api/helper/DynamoDBTestHelper.java
@@ -2,6 +2,7 @@ package kae.wasun.weather.api.helper;
 
 import kae.wasun.weather.api.model.document.WeatherTrackingDocument;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 import software.amazon.awssdk.enhanced.dynamodb.Key;
 
 public class DynamoDBTestHelper {
@@ -10,14 +11,17 @@ public class DynamoDBTestHelper {
 
     }
 
-    public static WeatherTrackingDocument getItem(DynamoDbEnhancedClient enhancedClient,
-                                                  String partitionKey,
-                                                  String sortKey) {
-        var dynamoDbTable = enhancedClient.table(
+    private static DynamoDbTable<WeatherTrackingDocument> getDynamoDBTable(DynamoDbEnhancedClient enhancedClient) {
+        return enhancedClient.table(
                 WeatherTrackingDocument.TABLE_NAME,
                 WeatherTrackingDocument.TABLE_SCHEMA
         );
+    }
 
+    public static WeatherTrackingDocument getItem(DynamoDbEnhancedClient enhancedClient,
+                                                  String partitionKey,
+                                                  String sortKey) {
+        var dynamoDbTable = getDynamoDBTable(enhancedClient);
         var key = Key.builder()
                 .partitionValue(partitionKey)
                 .sortValue(sortKey)
@@ -27,20 +31,12 @@ public class DynamoDBTestHelper {
     }
 
     public static void putItem(DynamoDbEnhancedClient enhancedClient, WeatherTrackingDocument document) {
-        var dynamoDbTable = enhancedClient.table(
-                WeatherTrackingDocument.TABLE_NAME,
-                WeatherTrackingDocument.TABLE_SCHEMA
-        );
-
+        var dynamoDbTable = getDynamoDBTable(enhancedClient);
         dynamoDbTable.putItem(document);
     }
 
     public static void deleteAllItems(DynamoDbEnhancedClient enhancedClient) {
-        var dynamoDbTable = enhancedClient.table(
-                WeatherTrackingDocument.TABLE_NAME,
-                WeatherTrackingDocument.TABLE_SCHEMA
-        );
-
+        var dynamoDbTable = getDynamoDBTable(enhancedClient);
         var pageIterable = dynamoDbTable.scan();
 
         pageIterable.stream().forEach(page ->
