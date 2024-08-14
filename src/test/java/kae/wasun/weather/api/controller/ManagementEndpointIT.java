9b13dbf961b755a3b0eb1c9e494867977e790363
Wasun Kaewpradub <wasun.kaewpradub@gmail.com>
2024-08-14 12:27:52 +0700

    Verify strict json from management endpoint integration test

--- src/test/java/kae/wasun/weather/api/controller/ManagementEndpointIT.java
+++ src/test/java/kae/wasun/weather/api/controller/ManagementEndpointIT.java
@@ -34,7 +34,7 @@ public class ManagementEndpointIT {
                         {
                             "status": "UP"
                         }
-                        """)
+                        """, true)
                 );
     }
 
